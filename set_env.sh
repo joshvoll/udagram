00e70302c4ebab19f8e70ad04e856f048fed8ffa
josue rodriguez <joshvoll@yahoo.com>
2021-01-24 12:45:32 -0600

    adding all file to github

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=josue
+export POSTGRES_PASSWORD=josuemrc1234A
+export POSTGRES_HOST=cdr.c8gexecchxua.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-240299768454-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
