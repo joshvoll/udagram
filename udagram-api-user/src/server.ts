c511efac5fb605b0fb79b81c70320ed23d9cb15f
josue rodriguez <joshvoll@yahoo.com>
2021-01-27 10:52:11 -0600

    adding core

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -20,6 +20,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
   app.use(cors({
     allowedHeaders: [
+      'Access-Control-Allow-Origin': '*',
       'Origin', 'X-Requested-With',
       'Content-Type', 'Accept',
       'X-Access-Token', 'Authorization',
